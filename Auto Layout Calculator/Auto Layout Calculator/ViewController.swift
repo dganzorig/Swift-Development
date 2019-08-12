069a25bb3e3e74bdab5af1617b502265f33e5b40
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-08-11 22:46:10 -0400

    Adding Auto Layout Calculator Project

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Dona Ganzorig on 8/11/19.
+//  Copyright © 2019 Dona Ganzorig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
