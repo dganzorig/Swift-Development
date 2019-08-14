8cafd3462d0dd76f2b197e743815a50da0419e7d
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-08-13 23:35:07 -0400

    Adding Segues

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Dona Ganzorig on 8/13/19.
+//  Copyright © 2019 Dona Ganzorig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreen" {
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.textPassedOver = textField.text
+        }
+    }
+    
+}
+
